8e01a762e2fa83850a78f34a39c1c9e7b0301f96
Steffi-b-silly <72018183+Steffi-b-silly@users.noreply.github.com>
2020-11-14 18:33:21 +0100

    Add CSS and HTML/jsx from my previous project as part/start of transferring it from Vanilla JS into React

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from 'axios';
+import "./Weather.css";
 import Loader from 'react-loader-spinner';
 
 
@@ -13,6 +14,9 @@ export default function Weather(props) {
     
     axios.get(apiUrl).then(handleResponse);
     return (
+        <div className="container">
+      <div className="wrapper">
+        <div className="weather-app-border">
         <Loader
          type="ThreeDots"
          color="violet"
@@ -21,5 +25,54 @@ export default function Weather(props) {
          timeout={3000} //3 secs
  
       />
+      
+          <form id="search-form">
+            <div className="row">
+              <div className="col-9">
+                <input type="search" placeholder="Type a city..." className="form-control" id="city-input" />
+              </div>
+              <div className="col-3">
+                <input type="submit" value="Search" className="btn btn-primary w-100" />
+              </div>
+            </div>
+          </form>
+          <div className="overview">
+      <h1 id="city"></h1>
+      <ul>
+        <li>Last updated: <span id="date"></span></li>
+        <li id="description"></li>
+      </ul>
+          </div>
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix weather-temp">
+          <img src="" alt="" id="weather-icon" className="float-left" />
+          <div className="float-left">
+          <strong id="temp"></strong>
+            <span className="units">
+              <a href="#" id="celsius">°C</a> | 
+              <a href="#" id="fahrenheit">°F</a></span>
+          </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>
+              Humidity: <span id="humidity"></span> %
+            </li>
+            <li>
+              Wind: <span id="wind"> </span> km/h
+            </li>
+          </ul>
+        </div>  
+      </div>
+<div className="row weather-forecast" id="forecast"></div>
+</div>
+            <small className="steffi">
+              Open-source code by <a href="https://github.com/Steffi-b-silly/Steffis-weather-app_final-project"
+                target="_blank">Steffi Bayerl</a>
+            </small>
+     </div>
+    </div>
     )
 }
