7bd3ac4e3225a0e8d5337c20cf03fbbe37791f6c
Steffi-b-silly <72018183+Steffi-b-silly@users.noreply.github.com>
2020-11-19 18:56:49 +0100

    Made some changes to the HTML/jsx

--- src/Weather.js
+++ src/Weather.js
@@ -26,10 +26,10 @@ export default function Weather(props) {
  
       />
       
-          <form id="search-form">
+          <form>
             <div className="row">
               <div className="col-9">
-                <input type="search" placeholder="Type a city..." className="form-control" id="city-input" />
+                <input type="search" placeholder="Type a city..." className="form-control" />
               </div>
               <div className="col-3">
                 <input type="submit" value="Search" className="btn btn-primary w-100" />
@@ -37,18 +37,18 @@ export default function Weather(props) {
             </div>
           </form>
           <div className="overview">
-      <h1 id="city">Your city</h1>
+      <h1>Your city</h1>
       <ul>
-        <li>Last updated: <span id="date">Sunday, 18:15 o'clock</span></li>
-        <li id="description">Clear sky</li>
+        <li>Last updated: <span>Sunday, 18:15 o'clock</span></li>
+        <li>Clear sky</li>
       </ul>
           </div>
       <div className="row">
         <div className="col-6">
           <div className="clearfix weather-temp">
-          <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="weather icon" id="weather-icon" className="float-left" />
+          <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="Weather icon" className="float-left" />
           <div className="float-left">
-          <strong id="temp">XY</strong>
+          <strong>XY</strong>
             <span className="units">
               <span>°C</span> | 
               <span>°F</span></span>
@@ -58,15 +58,15 @@ export default function Weather(props) {
         <div className="col-6">
           <ul>
             <li>
-              Humidity: <span id="humidity">XY</span> %
+              Humidity: <span>XY</span> %
             </li>
             <li>
-              Wind: <span id="wind">YZ</span> km/h
+              Wind: <span>YZ</span> km/h
             </li>
           </ul>
         </div>  
       </div>
-<div className="row weather-forecast" id="forecast">Forecast for the next 16 hours</div>
+<div className="row weather-forecast">Forecast for the next 16 hours</div>
 </div>
             <small className="steffi">
               Open-source code by <a href="https://github.com/Steffi-b-silly/react-weather-app"
